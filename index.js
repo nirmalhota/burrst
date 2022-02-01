7cb8fa76ea2eb9217182a2332d74cc17cb48b180
sagv7824 <a.sarthak7824@gmail.com>
2022-02-01 11:57:15 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const server = express();
-const PORT = 6556;
-
-server.use(express.static('./bradyn-swinson'));
-
-server.listen(PORT,() => {
-    console.log(`server is listening at PORT ${PORT}`);
-});
